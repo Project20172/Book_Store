e3ea23cfc312340e79de2b83f374ac065a59ded4
Nguyen Hoang Giang <giang.nh261197@gmail.com>
2018-05-05 22:57:33 +0700

    create content in homepage

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,13 @@ Route::get('add-to-cart/{id}', [
 
 Route::get('loai-san-pham/{id}','ViewPages@getLoaiSanPham')->name('loaisanpham');
 
+Route::get('/book_detail/{id}','WebManager@getBookDetail')->name('getBookDetail');
+
+Route::get('admin/next3book/{id}','WebManager@getNext3Book')->name('getNext3Book');
+
+Route::get('admin/nexttabvanhoc/{id}','WebManager@getNextTabVanHoc')->name('getNextTabVanHoc');
+
+Route::get('admin/prevtabvanhoc/{id}','WebManager@getPrevTabVanHoc')->name('getPrevTabVanHoc');
+
+Route::get('admin/prev3book/{id}','WebManager@getPrev3Book')->name('getPrev3Book');
+
