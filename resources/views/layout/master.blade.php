322e05843f66e1e2d14a7bf20b9589db8bccaae2
tienthienhd <tienthienhd@gmail.com>
2018-05-05 09:53:06 +0700

    init layout and base pages

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Book Store</title>
+	
+</head>
+<body>
+	@include('layout.elements.header1')
+	@yield('container')
+	@include('layout.elements.footer')
+</body>
+</html>
