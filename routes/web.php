412b3de72d6603df0c0b2ce1cdd6970aabef8de9
HGiang02 <37242751+HGiang02@users.noreply.github.com>
2018-05-06 13:15:50 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,55 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('pages.home');
+});
 
+Route::get('/admin','WebManager@getAdmin')->name('getAdmin');
 
+Route::get('/admin/add-category','WebManager@getAddCategory')->name('getAddCategory');
+
+Route::post('/admin/add-category','WebManager@postAddCategory')->name('postAddCategory');
+
+Route::get('/admin/add-book','WebManager@getAddBook')->name('getAddBook');
+
+Route::get('/admin/remove-book/{id}','WebManager@getRemoveBook')->name('getRemoveBook');
+
+Route::get('/admin/remove-author/{id}','WebManager@getRemoveAuthor')->name('getRemoveAuthor');
+
+Route::get('/admin/remove-category/{id}','WebManager@getRemoveCategory')->name('getRemoveCategory');
+
+Route::post('/admin/add-book','WebManager@postAddBook')->name('postAddBook');
+
+Route::get('/admin/list-book','WebManager@getListBook')->name('getListBook');
+
+Route::get('/admin/edit-book/{id}','WebManager@getEditBook')->name('getEditBook');
+
+Route::post('/admin/edit-book','WebManager@postEditBook')->name('postEditBook');
+
+Route::get('/admin/edit-category/{id}','WebManager@getEditCategory')->name('getEditCategory');
+
+Route::post('/admin/edit-category','WebManager@postEditCategory')->name('postEditCategory');
+
+Route::get('/admin/list-category','WebManager@getListCategory')->name('getListCategory');
+
+Route::get('admin/list-author','WebManager@getListAuthor')->name('getListAuthor');
+
+Route::get('admin/add-author','WebManager@getAddAuthor')->name('getAddAuthor');
+
+Route::post('admin/add-author','WebManager@postAddAuthor')->name('postAddAuthor');
+
+Route::get('admin/edit-author/{id}','WebManager@getEditAuthor')->name('getEditAuthor');
+
+Route::post('admin/edit-author','WebManager@postEditAuthor')->name('postEditAuthor');
+
+Route::get('loai-san-pham/{id}','ViewPages@getLoaiSanPham')->name('loaisanpham');
+
+Route::get('/cart', function () {
+    return view('pages.carts');
+})->name('cart');
 
 Route::get('/home', 'ViewPages@homepage')->name('home');
-
-
-
 
 Route::get('login', function () {
     return view('pages.login');
@@ -26,74 +68,11 @@ Route::get('/register', function () {
     return view('pages.register');
 })->name('register');
 
-
-Route::get('/cart', [
-	'as'=>'cart',
-	'uses'=>'ViewPages@getCart'
-]);
-
-Route::get('add-to-cart/{id}', [
-	'as' => 'book.addToCart',
-	'uses' => 'ViewPages@getAddToCart'
-]);
-
-
-
-Route::get('/book_detail/{id}','WebManager@getBookDetail')->name('getBookDetail');
-
-Route::get('/home', 'ViewPages@homepage')->name('home');
-
-
-Route::get('/admin','WebManager@getAdmin')->name('getAdmin');
-
-Route::get('/admin/list-category','WebManager@getListCategory')->name('getListCategory');
-
-Route::get('/admin/add-category','WebManager@getAddCategory')->name('getAddCategory');
-
-Route::get('admin/list-author','WebManager@getListAuthor')->name('getListAuthor');
-
-Route::get('admin/add-author','WebManager@getAddAuthor')->name('getAddAuthor');
-
-Route::get('/admin/list-book','WebManager@getListBook')->name('getListBook');
-
-Route::get('/admin/add-book','WebManager@getAddBook')->name('getAddBook');
-
-Route::get('/admin/edit-category/{id}','WebManager@getEditCategory')->name('getEditCategory');
-
-Route::post('/admin/add-category','WebManager@postAddCategory')->name('postAddCategory');
-
-Route::post('/admin/edit-category','WebManager@postEditCategory')->name('postEditCategory');
-
-Route::get('/admin/remove-category/{id}','WebManager@getRemoveCategory')->name('getRemoveCategory');
-
-Route::post('admin/add-author','WebManager@postAddAuthor')->name('postAddAuthor');
-
-Route::get('admin/edit-author/{id}','WebManager@getEditAuthor')->name('getEditAuthor');
-
-Route::post('admin/edit-author','WebManager@postEditAuthor')->name('postEditAuthor');
-
-Route::get('/admin/remove-author/{id}','WebManager@getRemoveAuthor')->name('getRemoveAuthor');
-
-Route::post('/admin/add-book','WebManager@postAddBook')->name('postAddBook');
-
-Route::get('/admin/edit-book/{id}','WebManager@getEditBook')->name('getEditBook');
-
-Route::post('/admin/edit-book','WebManager@postEditBook')->name('postEditBook');
-
-Route::get('/admin/remove-book/{id}','WebManager@getRemoveBook')->name('getRemoveBook');
-
-
-Route::get('loai-san-pham/{id}','ViewPages@getLoaiSanPham')->name('loaisanpham');
-
-Route::get('/book_detail/{id}','WebManager@getBookDetail')->name('getBookDetail');
-
-
-Route::get('admin/next3book/{id}','WebManager@getNext3Book')->name('getNext3Book');
-
-Route::get('admin/nexttabvanhoc/{id}','WebManager@getNextTabVanHoc')->name('getNextTabVanHoc');
-
-Route::get('admin/prevtabvanhoc/{id}','WebManager@getPrevTabVanHoc')->name('getPrevTabVanHoc');
-
-Route::get('admin/prev3book/{id}','WebManager@getPrev3Book')->name('getPrev3Book');
-
-
+Route::get('book_detail', function () {
+    return view('pages.book_detail');
+})->name('book_detail');
+Route::get('/user_information',function(){
+	return view('pages.user_information');
+})->name('user_information');
+Route::post('/home','RegisterController@addCustomer')->name('addCustomer');
+Route::get('/result-search', 'ViewPages@getResultSearch')->name('result-search');
